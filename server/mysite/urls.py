2dbbb18baeeda569d96523bdde1cf60c4918aad2
Sourav12061999 <sdunity12061999@gmail.com>
2023-06-01 22:36:30 +0530

    server

--- server/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import signup, signin
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('signup/', signup, name='signup'),
-    path('signin/', signin, name='signin'),
-]
