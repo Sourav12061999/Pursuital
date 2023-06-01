668825b79878be41715cf7b394c2254dfef826d8
Sourav Das <sdunity12061999@gmail.com>
2023-06-01 22:38:07 +0530

    server

--- server/mysite/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        app_label = 'mysite'
-        model=Us
-        fields="__all__"
