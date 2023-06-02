8c7c8eb8cac62a7e73ac2cc9035cfcd91d43ff05
Adarsha Khatua <adarshakhatua.ak@gmail.com>
2023-06-02 14:14:54 +0530

    home page css

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { SigninComponent } from './signin/signin.component';
-import { MilestoneFormComponent } from './milestone-form/milestone-form.component';
-import { SignupComponent } from './signup/signup.component';
-import { CampaignComponent } from './campaign/campaign.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SigninComponent,
-    MilestoneFormComponent,
-    SignupComponent,
-    CampaignComponent,
+    HomeComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
