e5558d4862799433c3d12d61d62d5adcfa4f0077
Sourav Das <sdunity12061999@gmail.com>
2023-06-02 16:06:13 +0530

    chache ignore

--- pursuital_server/pursuital_apis/views.py
+++ pursuital_server/pursuital_apis/views.py
@@ -84,3 +84,4 @@ class BatchCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
