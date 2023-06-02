cd5f4a9e9a930fc1d0584577f2a78d41ce136813
Sourav Das <sdunity12061999@gmail.com>
2023-06-02 16:51:21 +0530

    All The API Routes have been added

--- pursuital_server/pursuital_apis/views.py
+++ pursuital_server/pursuital_apis/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
-from .serializers import UserSerializer, BatchSerializer, CampaignSerializer
+from .serializers import UserSerializer, BatchSerializer, CampaignSerializer, CampaignUserSerializer, GoalSerializer, GoalUserSerializer, MilestoneSerializer, MilestoneUserSerializer
 from .models import Batch
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
